460ec183f64968db6ca0ab5f4656080fd732251d
yunior <yunior.varon@yahoo.com>
2021-09-28 17:57:41 +0200

    The script for the video player

---
+++ assets/videoPlayer.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+  href: "https://youtu.be/F0KN8cgHC8g",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+  autoPlayVideos: "true",
+});
